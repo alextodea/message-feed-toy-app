ee28eef1722c795dbaaab753974d7eb00206ff82
Alexandru Todea <alexandrutodea1992@gmail.com>
2019-02-17 12:37:23 +0100

    create react app in frontend folder

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+require('dotenv').config()
+const port = process.env.PORT || 4000;
 
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
