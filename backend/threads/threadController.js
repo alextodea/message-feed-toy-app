02268ec1fb9623e5e11a2ae11ed258ce042e0871
Alexandru Todea <alexandrutodea1992@gmail.com>
2019-02-25 08:11:49 +0100

    Finish implementing comments retrieval on user interface

--- backend/threads/threadController.js
+++ backend/threads/threadController.js
@@ -3,22 +3,36 @@ const User = require("../users/userSchema");
 const Comment = require("../comments/commentSchema");
 
 exports.getThreads = async (req,res) => {
-    const obj = {};
-    const threads = await Thread.find({}).populate("author").sort({createdDate:-1})
-    threads.forEach(async thread => {
-        obj[thread._id] = {
-            author: thread.author.email,
-            title: thread.title,
-            createdDate: thread.createdDate,
+    try {
+        let obj = {};
+        
+        const populateQuery = {
+            path: "author comments",
+            populate: { path: "author_id" }
         }
-        if (thread.comments === undefined || thread.comments.length === 0) return
-        obj[thread._id]["comments"] = [];
-        const threadComments = await Comment.find({'_id': { $in: thread.comments}}).populate("author_id thread_id");
-        obj[thread._id].comments.push("Sss")
-    })
-    res.status(500).json({message:"Internal error!"});
 
-    //// vezi dc nu se pushuiesc comentariile in obiect
+        const threads = await Thread.find({}).populate(populateQuery).sort({createdDate:-1});
+        threads.forEach(thread=> {
+            const {_id,createdDate,title} = thread;
+            const authorEmail = thread.author.email
+            obj[_id] = {title,createdDate,authorEmail}
+            if (thread.comments === undefined || thread.comments.length === 0) return
+
+            const mappedComments = thread.comments.map(comment => (
+                {
+                    author: comment.author_id.email,
+                    body: comment.body,
+                    createdDate: comment.createdDate
+                }
+            ));
+
+            obj[_id]["comments"] = mappedComments;
+        })
+
+        res.status(200).json(obj);
+    } catch(e) {
+        res.status(500).json({message:"Internal error!"});
+    }
 };
 
 exports.addThread = (req,res) => {
